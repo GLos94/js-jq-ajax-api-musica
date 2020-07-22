d0d4f8bd78b0217c59edcf355b3bc4439895df48
Giulia <giulialosito94@gmail.com>
2020-07-22 16:15:14 +0200

    Correzioni

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
 function callPosters() {
+  var dischi = [];
   $.ajax({
 
     url: 'https://flynn.boolean.careers/exercises/api/array/music',
@@ -17,31 +17,31 @@ function callPosters() {
     }
 
 
-
-
-
   });
 }
 
 function drawPoster(dischi) {
+
   var template = $('#poster-template').html();
   var compiled = Handlebars.compile(template);
   var target = $('.cds-container container');
 
+  for (var i = 0; i < dischi.length; i++) {
+    var disco = dischi[i];
+    disco =
+    {
+    'poster': disco['poster'],
+    'title' : disco['title'],
+    'author': disco['author'],
+    'genre': disco['genre'],
+    'year': disco['year']
+  };
 
-  var disco = dischi[i];
-
-  disco = {
-    "poster": "",
-    "title": "",
-    "author": "",
-    "year": ""
-  },
 
   target.append(disco);
 }
 
-
+}
 
 
 function init(){
